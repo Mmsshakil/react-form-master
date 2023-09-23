872763e47264757a066163bc4b1076208d6b60be
Mmsshakil <softhandshakil@gmail.com>
2023-09-23 17:10:51 +0600

    preventDefault added.. for this page will not reloaded

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,12 +1,19 @@
 
 
 const SimpleForm = () => {
+
+    const handleSubmit = e =>{
+        
+        // for this line the page will not reload
+        e.preventDefault();
+        console.log('form submited');
+    }
     return (
         <div>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <input type="text" />
                 <br />
-                <button>Submit</button>
+                <input type="submit" value="Submit" />
             </form>
             
         </div>
