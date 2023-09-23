cf1f57cf9ec5c4cb06d7f763c25c54c744f820a2
Mmsshakil <softhandshakil@gmail.com>
2023-09-23 17:51:22 +0600

    get the input value by e.target.name.value

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -3,15 +3,23 @@
 const SimpleForm = () => {
 
     const handleSubmit = e =>{
-        
+
         // for this line the page will not reload
         e.preventDefault();
-        console.log('form submited');
+        // this line will show the input value (e.target.name.value)
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+        // console.log('form submited');
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" />
+                <input type="text" name="name" placeholder="Name..." />
+                <br />
+                <input type="email" name="email" id="" placeholder="Email..." />
+                <br />
+                <input type="text" name="phone" placeholder="Phone..." />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
